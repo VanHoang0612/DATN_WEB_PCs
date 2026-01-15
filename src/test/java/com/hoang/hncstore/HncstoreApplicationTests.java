42f8fc3dae9e412481511d1653be5809ac2f9193
VanHoang0612 <nguyenvanhoang031206@gmail.com>
2026-01-15 10:14:12 +0700

    Khởi tạo dự án

---
+++ src/test/java/com/hoang/hncstore/HncstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoang.hncstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HncstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
