c7ec0bc0ca8273db1fbdced560ce155e7cea47a5
VanHoang0612 <nguyenvanhoang031206@gmail.com>
2026-01-20 09:27:25 +0700

    Cấu hình MessageSource cho lỗi validate và thuật toán mã hóa mật khẩu

---
+++ src/main/java/com/hoang/hncstore/configuration/AppConfig.java
@@ -0,0 +1,24 @@
+package com.hoang.hncstore.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages_validate");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
