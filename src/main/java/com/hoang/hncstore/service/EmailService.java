16dfaef137e69faff0fddcac19dc6e17a7e21f3e
VanHoang0612 <nguyenvanhoang031206@gmail.com>
2026-01-22 20:38:39 +0700

    Thêm dịch vụ gửi email

---
+++ src/main/java/com/hoang/hncstore/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.hoang.hncstore.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public void sendOtpEmail(String to, String subject, String body) {
+        // Implementation for sending OTP email
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+
+    }
+}
